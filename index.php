7b91382d5e981915682251d4e95286eb6ef6f90c
Krisna Ajie Prasetyo <krisnaajiep@gmail.com>
2024-12-30 19:24:55 +0700

    feat: add article section with several example of articles for home page

--- index.php
+++ index.php
@@ -12,6 +12,20 @@
   <header>
     <h1>Personal Blog</h1>
   </header>
+
+  <!--Article Section  -->
+  <article>
+    <h2><a href="#">My first article</a></h2>
+    <h3>August 7, 2024</h3>
+  </article>
+  <article>
+    <h2><a href="#">Second article</a></h2>
+    <h3>August 4, 2024</h3>
+  </article>
+  <article>
+    <h2><a href="#">Third article</a></h2>
+    <h3>August 1, 2024</h3>
+  </article>
 </body>
 
 </html>
