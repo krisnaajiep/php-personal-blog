d59ab3b09649af520d51a29297dee329cae3a0e4
Krisna Ajie Prasetyo <krisnaajiep@gmail.com>
2024-12-31 01:24:04 +0700

    feat: create `article page` in article.php to display the content of the article

---
+++ article.php
@@ -0,0 +1,14 @@
+<?php include 'includes/head.php' ?>
+
+<!-- Header Section -->
+<header>
+  <h1>My first article</h1>
+  <h3>August 7, 2024</h3>
+</header>
+
+<!-- Main Section -->
+<main>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. A sequi sit, quis ducimus minima sunt repellendus error, officia dicta voluptas magni fuga fugiat libero reiciendis quisquam! Consequatur, dolor magnam itaque voluptatem dolores distinctio libero voluptatum est, odio totam sit, ut nobis impedit voluptas? Numquam error nam qui laborum dolorum, non accusantium veniam debitis pariatur velit vero ipsam nihil magni blanditiis sapiente temporibus commodi doloremque repellat alias. Velit iste est tenetur numquam culpa consectetur harum magni quaerat sint itaque sunt minima eligendi, adipisci repellat ab deleniti ullam necessitatibus beatae dignissimos et. Assumenda, sapiente! Nostrum maiores harum totam dolorem quo praesentium voluptatem.</p>
+
+  <a href="index.php" class="back">Go back</a>
+</main>
