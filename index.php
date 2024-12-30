7db62525e21cb68f071bcd132814f6014a95ae80
Krisna Ajie Prasetyo <krisnaajiep@gmail.com>
2024-12-30 20:13:43 +0700

    refactor: wrap article sections into main sections in `index.php` file

--- index.php
+++ index.php
@@ -5,18 +5,20 @@ include 'includes/header.php';
 
 ?>
 
-<!--Article Section  -->
-<article>
-  <h2><a href="#">My first article</a></h2>
-  <h3>August 7, 2024</h3>
-</article>
-<article>
-  <h2><a href="#">Second article</a></h2>
-  <h3>August 4, 2024</h3>
-</article>
-<article>
-  <h2><a href="#">Third article</a></h2>
-  <h3>August 1, 2024</h3>
-</article>
+<!-- Main Section -->
+<main>
+  <article>
+    <h2><a href="#">My first article</a></h2>
+    <h3>August 7, 2024</h3>
+  </article>
+  <article>
+    <h2><a href="#">Second article</a></h2>
+    <h3>August 4, 2024</h3>
+  </article>
+  <article>
+    <h2><a href="#">Third article</a></h2>
+    <h3>August 1, 2024</h3>
+  </article>
+</main>
 
 <?php include 'includes/foot.php' ?>
