00a3831dbced855bc01fa968373b872fb406d369
Krisna Ajie Prasetyo <krisnaajiep@gmail.com>
2024-12-31 15:57:15 +0700

    feat: add label to input form in edit and add article pages

--- edit.php
+++ edit.php
@@ -9,12 +9,15 @@
 <main>
   <form action="" method="post">
     <div class="form-input">
+      <label for="article_title">Article Title</label>
       <input type="text" name="article_title" id="article_title" placeholder="Article Title">
     </div>
     <div class="form-input">
+      <label for="publishing_date">Publishing Date</label>
       <input type="date" name="publishing_date" id="publishing_date" placeholder="Publishing Date">
     </div>
     <div class="form-input">
+      <label for="content">Content</label>
       <textarea name="content" id="content" rows="10" placeholder="Content"></textarea>
     </div>
     <button type="submit" name="update">Update</button>
