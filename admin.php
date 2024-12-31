449d0ebd8c3e7dde6c21055e3fc82eb1e34326b2
Krisna Ajie Prasetyo <krisnaajiep@gmail.com>
2024-12-31 12:15:27 +0700

    feat: create dashboard in admin.php to display the list of articles published

---
+++ admin.php
@@ -0,0 +1,27 @@
+<?php include 'includes/head.php' ?>
+
+<header>
+  <h1>Personal Blog</h1>
+  <a href="#">+ Add</a>
+</header>
+
+<!-- Main Section -->
+<main>
+  <article>
+    <h3 class="title"><a href="article.php">My first article</a></h3>
+    <a href="#" class="edit">Edit</a>
+    <a href="#" class="delete">Delete</a>
+  </article>
+  <article>
+    <h3 class="title"><a href="#">Second article</a></h3>
+    <a href="#" class="edit">Edit</a>
+    <a href="#" class="delete">Delete</a>
+  </article>
+  <article>
+    <h3 class="title"><a href="#">Third article</a></h3>
+    <a href="#" class="edit">Edit</a>
+    <a href="#" class="delete">Delete</a>
+  </article>
+</main>
+
+<?php include 'includes/foot.php' ?>
